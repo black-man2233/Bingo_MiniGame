bccfedf258ba01548a3c05e60a72905d4336297b
Kevin Bamwesa Ishara <kevi3758@elev.techcollege.dk>
2022-09-20 13:55:26 +0200

    Trying to move delete used numbers

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace Bingo_Game
 {
@@ -20,9 +11,49 @@ namespace Bingo_Game
     /// </summary>
     public partial class MainWindow : Window
     {
+        Random rnd = new Random();
+        int a;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            for (int i = 1; i < 5 + (1); i++)
+            {
+                Ubrugte_ListBox.Items.Add(i);
+            }
+            MessageBox.Show($"{Ubrugte_ListBox.Items[0]}");
+
         }
+
+
+
+
+        private void Tryk_knap_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            a = rnd.Next(0, Ubrugte_ListBox.Items.Count);
+
+            if (Ubrugte_ListBox.Items.Contains(a))
+            {
+                Brugte_listBox.Items.Add(a);
+                Ubrugte_ListBox.Items.Remove(a);
+            }else 
+            {
+                a = rnd.Next(0, Ubrugte_ListBox.Items.Count);
+                Brugte_listBox.Items.Add(a);
+                Ubrugte_ListBox.Items.Remove(a);
+            }
+
+            Debug.WriteLine(a);
+
+        }
+
+
+
+
+
     }
 }
+
