71ab316eac6bba58bb43b3e6e1b21da0e4efeeb8
Kevin Bamwesa Ishara <kevi3758@elev.techcollege.dk>
2022-09-22 13:56:31 +0200

    IDK how to fix this

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Windows;
-
+using System.Linq;
+using StackExchange.Redis;
 
 namespace Bingo_Game
 {
@@ -11,49 +12,43 @@ namespace Bingo_Game
     /// </summary>
     public partial class MainWindow : Window
     {
-        Random rnd = new Random();
-        int a;
+        Random talRandomizer = new Random();
+        int trukketTal;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            for (int i = 1; i < 5 + (1); i++)
+            for (int i = 1; i < 10 + (1); i++)
             {
                 Ubrugte_ListBox.Items.Add(i);
             }
-            MessageBox.Show($"{Ubrugte_ListBox.Items[0]}");
+
 
         }
-
-
-
 
         private void Tryk_knap_Click(object sender, RoutedEventArgs e)
         {
 
-
-            a = rnd.Next(0, Ubrugte_ListBox.Items.Count);
-
-            if (Ubrugte_ListBox.Items.Contains(a))
+            if (!Ubrugte_ListBox.Items.IsEmpty)
             {
-                Brugte_listBox.Items.Add(a);
-                Ubrugte_ListBox.Items.Remove(a);
-            }else 
-            {
-                a = rnd.Next(0, Ubrugte_ListBox.Items.Count);
-                Brugte_listBox.Items.Add(a);
-                Ubrugte_ListBox.Items.Remove(a);
+                trukketTal = talRandomizer.Next(1, Ubrugte_ListBox.Items.Count);
+
+                if (!Brugte_listBox.Items.Contains(trukketTal))
+                {
+                    Brugte_listBox.Items.Add(trukketTal);
+                    Ubrugte_ListBox.Items.Remove(trukketTal);
+
+                    Trukket_Label.Content = trukketTal;
+                }
+                Debug.WriteLine(trukketTal);
+
             }
 
-            Debug.WriteLine(a);
+
 
         }
-
-
-
-
-
     }
 }
+
 
